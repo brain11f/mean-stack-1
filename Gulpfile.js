8c3c3ea2085993b3801188764c4b604112e91731
Brian Finck <brian@brianfinck.com>
2015-10-08 12:01:04 -0700

    updating

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp'),
 		sass = require('gulp-sass'),
 		neat = require('node-neat'),
 		browserify = require('gulp-browserify'),
-		uglify = require(‘gulp-uglify’);
+		uglify = require('gulp-uglify');
 
-gulp.task(‘browserify’, function () {
-	return gulp.src([‘./src/*.js’])
+gulp.task('browserify', function () {
+	return gulp.src(['./src/*.js'])
    .pipe(browserify())
    .pipe(uglify())
-   .pipe(gulp.dest(‘./dist’));
+   .pipe(gulp.dest('./dist'));
 });
 
 var paths = {
