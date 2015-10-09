918d44d795e3a46a2ab2c886b656f3db6c5dd17c
Alina To <alina.to@gmail.com>
2015-10-09 14:25:30 -0700

    Add gulp-csslint and gulp-jshint to dev dependencies.

--- app/scripts/blog_details.directive.js
+++ app/scripts/blog_details.directive.js
@@ -1,4 +1,4 @@
-require("../app")
+require("../app");
 
 angular.module("AwesomeBlog")
 	.directive("bl", function () {
--- app/scripts/blogposts.ctrl.js
+++ app/scripts/blogposts.ctrl.js
@@ -1,5 +1,5 @@
 (function() {
-  'use strict'
+  'use strict';
 
   angular.module("AwesomeBlog").controller("BlogpostsCtrl", ["BlogpostService", function(BlogpostService) {
     var vm = this;
