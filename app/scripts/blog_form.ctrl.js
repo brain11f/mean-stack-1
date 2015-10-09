2293b6eca45ac0a21629a9b6c1c591b92e878668
Chris Holt <christopher.d.holt@gmail.com>
2015-10-09 16:54:13 -0700

    adjusted time on blog page, adjusted layout for 'delete' buttons and blog articles, adjusted padding to better match comp, moved flexbox mixins to _globalMixins

--- app/scripts/blog_form.ctrl.js
+++ app/scripts/blog_form.ctrl.js
@@ -11,12 +11,12 @@
       start();
 
       // IF statement only works when edit
-      function start() {
-        vm.blogpost.date = new Date(Date.now());
-        if ($routeParams.blogpost_id) {
-          BlogpostService.get($routeParams.blogpost_id).then(function(resp) {
-          vm.blogpost = resp.data;
-          vm.blogpost.date = vm.blogpost.date || new Date(Date.now());
+    function start() {
+      vm.blogpost.date = new Date(Date.now());
+      if ($routeParams.blogpost_id) {
+        BlogpostService.get($routeParams.blogpost_id).then(function(resp) {
+        vm.blogpost = resp.data;
+        vm.blogpost.date = vm.blogpost.date || new Date(Date.now());
         });
       }
     }
